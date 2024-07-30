a7900c6371b24f40eb7c71cc2a8e564447254bbb
Alexpm27 <213523@ids.upchiapas.edu.mx>
2024-07-30 04:07:54 -0600

    [UPDATE]

--- src/main/java/com/example/matchservice/config/WebClientConfig.java
+++ src/main/java/com/example/matchservice/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().baseUrl("http://sortir-249814840.us-east-1.elb.amazonaws.com")
+        return WebClient.builder().baseUrl("http://Sortir-load-balancer-391724916.us-east-1.elb.amazonaws.com")
                 .build();
     }
 }
